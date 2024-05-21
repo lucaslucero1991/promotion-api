cf7068a49f58b1a33db41caf1b0ad991e0f0bf28
User <user@hackerrank.workspace>
2024-05-21 23:11:56 +0000

    Add initial repository
    Commit by HackerRank

---
+++ CalendarWebApi/Controllers/CalendarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CalendarWebApi.DataAccess;
+using CalendarWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CalendarWebApi.DTO;
+using Mapster;
+
+namespace CalendarWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/calendar")]
+    public class CalendarController : ControllerBase
+    {
+        private readonly ILogger<CalendarController> _logger;
+
+        public CalendarController(ILogger<CalendarController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}
---
+++ CalendarWebApi/Services/CalendarService.cs
@@ -0,0 +1,43 @@
+﻿using CalendarWebApi.DTO;
+using CalendarWebApi.Models;
+
+namespace CalendarWebApi.Services
+{
+    public class CalendarService : ICalendarService
+    {
+        public Task<Calendar> AddEvent(CreateForm calendarEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Calendar> DeleteEvent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Calendar>> GetCalendar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Calendar> GetCalendar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Calendar>> GetCalendar(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Calendar>> GetEventsSorted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Calendar> UpdateEvent(int id, UpdateForm calendarEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
