48534af3dfb54ed9ee65053d9bc9cde232c17f36
llucero <lucaslucero1991@gmail.com>
2024-05-21 21:36:13 -0300

    add create calendar method

--- CalendarWebApi/Controllers/CalendarController.cs
+++ CalendarWebApi/Controllers/CalendarController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using CalendarWebApi.DataAccess;
+﻿using CalendarWebApi.DTO;
 using CalendarWebApi.Models;
+using CalendarWebApi.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using CalendarWebApi.DTO;
-using Mapster;
 
 namespace CalendarWebApi.Controllers
 {
@@ -15,12 +10,36 @@ namespace CalendarWebApi.Controllers
     public class CalendarController : ControllerBase
     {
         private readonly ILogger<CalendarController> _logger;
+        private readonly ICalendarService _calendarService;
 
-        public CalendarController(ILogger<CalendarController> logger)
+        public CalendarController(ILogger<CalendarController> logger,
+            ICalendarService calendarService)
         {
             _logger = logger;
+            _calendarService = calendarService;
         }
 
-       
+        [HttpPost]
+        public IActionResult CreateEvent([FromBody] Calendar? calendar)
+        {
+            if (calendar == null)
+            {
+                return BadRequest("calendar body is null");
+            }
+
+            var formDTO = new CreateForm
+            {
+                Name = calendar.Name,
+                Location = calendar.Location,
+                EventOrganizer = calendar.EventOrganizer,
+                Members = calendar.Members,
+                Time = calendar.Time
+            };
+
+            var response = _calendarService.AddEvent(formDTO);
+            
+            return CreatedAtAction(nameof(CreateEvent),
+                new { id = response.Id }, response);
+        }
     }
 }
--- CalendarWebApi/Services/CalendarService.cs
+++ CalendarWebApi/Services/CalendarService.cs
@@ -1,13 +1,30 @@
-﻿using CalendarWebApi.DTO;
+﻿using CalendarWebApi.DataAccess;
+using CalendarWebApi.DTO;
 using CalendarWebApi.Models;
 
 namespace CalendarWebApi.Services
 {
     public class CalendarService : ICalendarService
     {
+        private readonly IRepository _repository;
+        
+        public CalendarService(IRepository repository)
+        {
+            _repository = repository;
+        }
         public Task<Calendar> AddEvent(CreateForm calendarEvent)
         {
-            throw new NotImplementedException();
+            var calendar = new Calendar
+            {
+                Name = calendarEvent.Name ?? string.Empty,
+                Location = calendarEvent.Location ?? string.Empty,
+                Time = calendarEvent.Time ?? calendarEvent.Time.Value,
+                EventOrganizer = calendarEvent.EventOrganizer ?? string.Empty,
+                Members = calendarEvent.Members ?? string.Empty,
+
+            };
+
+            return _repository.AddEvent(calendar);
         }
 
         public Task<Calendar> DeleteEvent(int id)
